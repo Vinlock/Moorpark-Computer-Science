71c2843d0f43e1b5a94f41140d7db80178d94eef
Vinlock <Vinlock@users.noreply.github.com>
2015-09-29 18:18:14 -0700

    Create Homework-2.cpp
    Moorpark College / Intro to C++ / Professor Robert Farrell / Homework #2

---
+++ Homework/Homework-2.cpp
@@ -0,0 +1,16 @@
+// This program allows the user to input two floating point values and then calculates the sum, difference, product, and quotient of the numbers.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main2() {
+
+	//declare the two variables for the values
+	float pi = 3.14159265359;
+	double pid = 3.14159265359;
+
+	cout << pi << endl;
+	cout << pid << endl;
+
+}
