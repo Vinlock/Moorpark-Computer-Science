85ab2b1abf634c3c7553938163108153dc4d28f6
Vinlock <Vinlock@users.noreply.github.com>
2015-10-11 08:50:26 -0700

    Update Assignment-3.cpp

--- Programming-Assignments/Assignment-3.cpp
+++ Programming-Assignments/Assignment-3.cpp
@@ -29,8 +29,8 @@ const string READ_FILE = "RawScore.dat", WRITE_FILE = "ComputedScore.dat"; // De
 
 // Prototypes
 int Solved(int score);
-void ReadInputRecord(ifstream &, int &, int &, int &, int &, int &, int &, string &);
-void WriteInputRecord(ofstream &, int, string, int, int);
+bool ReadInputRecord(ifstream &, int &, int &, int &, int &, int &, int &, string &);
+bool WriteInputRecord(ofstream &, int, string, int, int);
 int CountLines(string);
 
 int main() {
@@ -68,23 +68,41 @@ int main() {
 				// For each line, read contents, then convert to desired output and write.
 				for (int i = ZERO; i < numLines; i++) {
 					cout << "Reading line." << endl;
-					ReadInputRecord(filename, score1, score2, score3, attempt1, attempt2, attempt3, teamName); // Read the contents of 'filename' and store it into the reference variables.
-					cout << "Team: " << teamName << " found." << endl;
-					cout << "Calculating number of successfully solved problems by " << teamName << "." << endl;
-					numSolved = Solved(score1) + Solved(score2) + Solved(score3); // Find out how many were solved by the team.
-					cout << teamName << " solved " << numSolved << " problems." << endl;
-					cout << "Calculating total time it took " << teamName << " to solve " << numSolved << " problems." << endl;
-					totalTime = score1 + score2 + score3; // Fine the total time that it took for them to solve all problems.
-					cout << teamName << " took " << totalTime << " seconds to solve " << numSolved << " problems." << endl;
-					iteration++; // Iterate for the first column's number.
-					cout << "Writing data to " << WRITE_FILE << endl;
-					WriteInputRecord(newFilename, iteration, teamName, numSolved, totalTime); // Write the new output to 'newFilename'.
-					cout << "Data written to " << WRITE_FILE << endl << endl;
+					// Read the contents of 'filename' and store it into the reference variables.
+					if (ReadInputRecord(filename, score1, score2, score3, attempt1, attempt2, attempt3, teamName)) {
+						cout << "Line successfully read" << endl; // Say the line is read if it didn't fail.
+						cout << "Team: " << teamName << " found." << endl; // Say the name of the team it found on the current line.
+						cout << "Calculating number of successfully solved problems by " << teamName << "." << endl;
+						numSolved = Solved(score1) + Solved(score2) + Solved(score3); // Find out how many were solved by the team.
+						cout << teamName << " solved " << numSolved << " problems." << endl; // Output number of solved problems by the team on the current line.
+						cout << "Calculating total time it took " << teamName << " to solve " << numSolved << " problems." << endl;
+						totalTime = score1 + score2 + score3; // Fine the total time that it took for them to solve all problems.
+						cout << teamName << " took " << totalTime << " seconds to solve " << numSolved << " problems." << endl; // Output the total time it took the team on the current line.
+						iteration++; // Iterate for the first column's number.
+						cout << "Writing data to " << WRITE_FILE << endl;
+						if (WriteInputRecord(newFilename, iteration, teamName, numSolved, totalTime)) { // Write the new output to 'newFilename'.
+							cout << "Data written to " << WRITE_FILE << endl << endl; // Output that the data was successfully written.
+						}
+						else {
+							cout << "Error writing data to file." << endl << endl; // If failed to write, output an error.
+							break; // Break the loop due to error.
+						}
+					}
+					else {
+						cout << "Error reading line." << endl << endl; // If failed to write, output an error.
+						break; // Break the loop due to error.
+					}
 				}
 
 				cout << "Closing " << WRITE_FILE << endl;
 				newFilename.close(); // Close the WRITE_FILE;
-				cout << WRITE_FILE << " closed." << endl << endl;
+				if (newFilename.is_open()) {
+					cout << "Error closing file." << endl << endl; // If failed to close, output an error.
+					return 0; // If failed to close, end program.
+				}
+				else {
+					cout << WRITE_FILE << " closed." << endl << endl;
+				}
 			}
 			else {
 				// If 'newFilename' failed to open, display an error message.
@@ -93,7 +111,13 @@ int main() {
 
 			cout << "Closing " << READ_FILE << endl;
 			filename.close(); // Close the READ_FILE.
-			cout << READ_FILE << " closed." << endl << endl ;
+			if (filename.is_open()) {
+				cout << "Error closing file." << endl << endl; // If failed to close, output an error.
+				return 0; // If failed to close, end program.
+			}
+			else {
+				cout << READ_FILE << " closed." << endl << endl;
+			}
 		}
 		else {
 			// If 'filename' failed to open, display an error message.
@@ -105,14 +129,24 @@ int main() {
 
 }
 
-void ReadInputRecord(ifstream &filename, int &score1, int &score2, int &score3, int &attempt1, int &attempt2, int &attempt3, string &teamName) {
+bool ReadInputRecord(ifstream &filename, int &score1, int &score2, int &score3, int &attempt1, int &attempt2, int &attempt3, string &teamName) {
 	// Change the values of the reference variables to be the contents of the current line.
-	filename >> score1 >> score2 >> score3 >> attempt1 >> attempt2 >> attempt3 >> teamName;
+	if (filename >> score1 >> score2 >> score3 >> attempt1 >> attempt2 >> attempt3 >> teamName) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
-void WriteInputRecord(ofstream &newFilename, int iteration, string teamName, int numSolved, int totalTime) {
+bool WriteInputRecord(ofstream &newFilename, int iteration, string teamName, int numSolved, int totalTime) {
 	// Write a new line to the 'newFilename' with the desired values and formatting. Also, sets the correct column widths for each column.
-	newFilename << setw(COLUMN1) << iteration << ": " << setw(COLUMN2) << left << teamName << right << " " << setw(COLUMN3) << numSolved << " " << setw(COLUMN4) << totalTime << endl;
+	if (newFilename << setw(COLUMN1) << iteration << ": " << setw(COLUMN2) << left << teamName << right << " " << setw(COLUMN3) << numSolved << " " << setw(COLUMN4) << totalTime << endl) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 int Solved(int score) {
