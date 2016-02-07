d41f12a65c3146e0705f05f87a6375f7a4dfa747
Vinlock <Vinlock@users.noreply.github.com>
2016-02-07 12:46:39 -0800

    Create Mortgage.h

---
+++ M10B/Homework/Homework-3/Multi-Files-Version/Mortgage.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#define Mortgage_h
+
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+class Mortgage {
+
+private:
+
+	/* **************************************************
+	Class Constants
+	************************************************** */
+	const float NUM_MONTHS = 12.0;  // Number of months in a year.
+	const float ONE = 1.0;          // Const for one.
+	const float HUNDRED = 100.0;    // Const for one hundred.
+
+	/* **************************************************
+	String Error constants for each possible error.
+	************************************************** */
+	const string ERR_AMOUNT = "Invalid Amount!";
+	const string ERR_RATE = "Invalid Interest Rate!";
+	const string ERR_YEARS = "Invalid Years!";
+
+	/* **************************************************
+	Class member variables
+	************************************************** */
+	float monthlyPayment = 0.0;     // Monthly Payment.
+	float loanAmount = 0.0;         // The dollar amount of the loan.
+	float interestRate = 0.0;       // The annual interest rate.
+	float numYears = 0.0;           // The number of years of the loan.
+
+
+	void MonthlyPayment();
+
+	float PeriodInterestRate();
+
+	float Term();
+
+	void ThrowError(const string ERROR);
+
+	float GetMonthInterest(float balance);
+
+	float GetMonthPrincipal(float balance);
+
+public:
+
+	bool SetLoanAmount(float amount);
+
+	bool SetInterestRate(float rate);
+
+	bool SetYears(float years);
+
+	float GetMonthlyPayment();
+
+	void PrintMonthlyPayment();
+
+	void PrintStatement();
+
+	void AskUser();
+
+};
